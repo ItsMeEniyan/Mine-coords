9678aeede708bc6e3a09cddd9f52d9fb41678ec7
ItsMeEniyan <57259374+ItsMeEniyan@users.noreply.github.com>
2021-05-15 12:45:18 +0530

    heroku deploy fix

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
+const path = require("path");
 const cors = require("cors");
 app.use(cors());
 
-const URI =process.env.MONGODBURL;
+const URI = process.env.MONGODBURL;
 
 const connectfun = async () => {
   await mongoose.connect(URI, {
@@ -25,6 +26,16 @@ app.use(express.json());
 
 const worldRouter = require("./Routes/world");
 app.use("/world", worldRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "/../frontend", "build", "index.html")
+    );
+  });
+}
 
 app.listen(process.env.PORT | 9000, function () {
   console.log("Server started.......");
